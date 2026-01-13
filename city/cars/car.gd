d0f3f89c7db63c0e64ab7e0b62d32a27e434e454
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-01-13 18:30:41 -0300

    update cars ai

--- city/cars/car.gd
+++ city/cars/car.gd
@@ -4,6 +4,7 @@ class_name Car
 static var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 @onready var car_model : CarModel = $cars
+@onready var shape_cast : ShapeCast3D = $cars/ShapeCast3D
 
 @export var speed : float = 20.0
 
@@ -14,4 +15,11 @@ func _ready() -> void:
 		car_model.rotation_degrees.y = 180
 
 func _physics_process(delta: float) -> void:
+	
+	for i in shape_cast.get_collision_count():
+		print(shape_cast.get_collider(i) is CharacterBody3D)
+		if shape_cast.get_collider(i) is CharacterBody3D:
+			return
+	
 	progress += speed * delta
+	
